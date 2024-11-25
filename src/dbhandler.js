b2fe2b89d3be623cabb3d22510152f471176c871
AlvinChrt <152682677+AlvinChrt@users.noreply.github.com>
2024-11-25 15:25:07 +0700

    Update dbhandler.js for google cloud

--- src/dbhandler.js
+++ src/dbhandler.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'localhost',     // Host database kamu
-    user: 'root',          // User database
-    password: '',  // Password database
-    database: 'calobite.sql' // Nama database
+  host: process.env.MYSQL_HOST || '34.128.92.233',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '123calobite123',
+  database: process.env.MYSQL_DATABASE || 'calobite',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = db;
