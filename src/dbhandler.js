49879daa6c7ae65a2672f7e2107c4924e83ea4d1
AlvinChrt <152682677+AlvinChrt@users.noreply.github.com>
2024-12-10 22:46:32 +0700

    Update dbhandler.js

--- src/dbhandler.js
+++ src/dbhandler.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST || '34.128.92.233',
+  host: process.env.MYSQL_HOST || '34.50.94.179',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '123calobite123',
   database: process.env.MYSQL_DATABASE || 'calobite',
